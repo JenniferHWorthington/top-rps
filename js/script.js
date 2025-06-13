226184c0ad3382713d9ae16338a767d6f5e63c1c
Jennifer Worthington <157091964+JenniferHWorthington@users.noreply.github.com>
2025-06-13 00:24:20 -0700

    Write function for playing a round and variables for the human and computer selections

--- js/script.js
+++ js/script.js
@@ -21,5 +21,38 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === "rock") {
+    if (computerChoice === "rock") {
+      return "Tie!!";
+    } else if (computerChoice === "paper") {
+      return "You lose!! Paper beats rock!";
+    } else if (computerChoice === "scissors") {
+      return "You win!! Rock beats scissors";
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      return "You win!! Paper beats rock!";
+    } else if (computerChoice === "paper") {
+      return "Tie!!";
+    } else if (computerChoice === "scissors") {
+      return "You lose!! Scissors beats paper!";
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "You lose!! Rock beats scissors!";
+    } else if (computerChoice === "paper") {
+      return "You win!! scissors beats paper!";
+    } else if (computerChoice === "scissors") {
+      return "Tie!!";
+    }
+  } else {
+    return "error";
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
